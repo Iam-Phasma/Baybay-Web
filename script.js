420f882286d861bc3109b9ae33503eb33d654cbd
Mad.RicsonJay <72855357+Iam-Phasma@users.noreply.github.com>
2024-02-03 21:47:30 +0800

    code has been cleaned and rearranged

--- script.js
+++ script.js
@@ -29,33 +29,6 @@ document.onkeydown = e => {
     }
 }
 
-//Trail for mouse and touch
-// document.addEventListener("DOMContentLoaded", function () {
-//     const mouseTrail = document.getElementById("mouse-trail");
-
-//     function addGlowTrail(e) {
-//         const glowTrail = document.createElement("div");
-//         glowTrail.style.position = "absolute";
-//         glowTrail.style.left = (e.pageX || e.touches[0].pageX) + "px";
-//         glowTrail.style.top = (e.pageY || e.touches[0].pageY) + "px";
-//         glowTrail.classList.add("mouse-glow");
-
-//         // Append the glow trail to the trail
-//         mouseTrail.appendChild(glowTrail);
-
-//         // Remove glow trail after the animation duration
-//         setTimeout(() => {
-//             glowTrail.remove();
-//         }, 500); // Adjusted the timeout to 500ms
-//     }
-
-//     document.addEventListener("mousemove", addGlowTrail);
-//     document.addEventListener("touchmove", function (e) {
-//         addGlowTrail(e.touches[0]);
-//     });
-// });
-
-
 //For ALPHA animation
 document.addEventListener("DOMContentLoaded", function () {
     const alphaAnimation = document.getElementById("alphaAnimation");
