a93a6b2d8a4d410f772a9e3802ac5939ea5209ee
Mad.RicsonJay <72855357+Iam-Phasma@users.noreply.github.com>
2024-05-04 10:07:27 +0800

    chore: Update app instructions and fix minor styling issues

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 document.oncontextmenu = () => {
-  alert("The codes are simple enough to be hidden :)");
+  //alert("The codes are simple enough to be hidden :)");
   return false;
 };
 
 document.onkeydown = (e) => {
   // Prevent F12 key
   if (e.key == "F12") {
-    alert("The codes are simple enough to be hidden :)");
+    //alert("The codes are simple enough to be hidden :)");
     return false;
   }
 
   // Prevent showing page source by ctrl U
   if (e.ctrlKey && e.key == "u") {
-    alert("The codes are simple enough to be hidden :)");
+    //alert("The codes are simple enough to be hidden :)");
     return false;
   }
 
   // Prevent copying from the page
   if (e.ctrlKey && e.key == "c") {
-    alert("Hello, there's nothing to copy here :)");
+    //alert("Hello, there's nothing to copy here :)");
     return false;
   }
 
   // Prevent pasting on the page
   if (e.ctrlKey && e.key == "v") {
-    alert("Hello, there's no where you can paste it here :)");
+    //alert("Hello, there's no where you can paste it here :)");
     return false;
   }
 };
@@ -32,7 +32,7 @@ document.onkeydown = (e) => {
 //For ALPHA animation
 document.addEventListener("DOMContentLoaded", function () {
   const alphaAnimation = document.getElementById("alphaAnimation");
-  const word = "Alpha Preview";
+  const word = "Release Preview";
   let counter = 0;
   let scramble = setInterval(() => {
     let randomText = Math.random().toString(36).substring(7).toUpperCase();
